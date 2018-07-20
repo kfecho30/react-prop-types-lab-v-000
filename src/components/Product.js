abe8051db5a6e98507108c5864c27dec56f014a2
Kelsey Fecho <kfecho30@gmail.com>
2018-07-20 20:34:20 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types';
 export default class Product extends React.Component {
   render() {
     return (
-      <h1>{this.prop.name}</h1>: a string — required
-      <p>{this.prop.producer}</p>: a string — optional
-hasWatermark: a boolean — optional, defaults to false
-color: a string — required, can only be 'white', 'eggshell-white' or 'salmon'
-weight: a number — required, ranges between 80 and 300
+      <h1>{this.prop.name}</h1> a string — required
+      <p>{this.prop.producer}</p> a string — optional
+      <p>{this.prop.hasWatermark}</p> a boolean — optional, defaults to false
+      <p>{this.prop.color}</p> a string — required, can only be 'white', 'eggshell-white' or 'salmon'
+      <p>{this.prop.weight}</p>: a number — required, ranges between 80 and 300
     )
   }
 }
 
-
+Product.defaultProps = {
+  hasWatermark: false
+}
